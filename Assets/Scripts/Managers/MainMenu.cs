7f23b4c085507944a9e1088cf82125f192e17fa3
TaynaCT <taynahsm@gmail.com>
2018-01-08 09:13:30 -0300

    UI - partI Ainda há optimizações pra fazer mas ta a funcionar

--- Assets/Scripts/Managers/MainMenu.cs
+++ Assets/Scripts/Managers/MainMenu.cs
@@ -1,12 +1,30 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
-    public void StartGame()
+    public GameObject LoadPanel;
+    public Slider LoadingBar;
+
+    public void LoadLevel(string sceneName)
     {
-        SceneManager.LoadScene("GamePlay");
+        StartCoroutine(LoadScene(sceneName));
+    }
+    
+    IEnumerator LoadScene(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        LoadPanel.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            //animação de espera enquanto a cena é carregada
+            LoadingBar.value = progress;
+
+            yield return null;
+        }
     }
 }
---
+++ Assets/Scripts/UI/BackToMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class BackToMenu : MonoBehaviour {
+
+	// Use this for initialization
+	public void Back()
+    {        
+        SceneManager.LoadScene("StartMenu");
+    }
+}
